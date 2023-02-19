fa42534eb3911291579556da225fa73d3e455445
shanekeane <shanekeane2001@yahoo.com>
2023-02-19 01:01:33 +0000

    xyz

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo committing to: $1
 echo commit name: $2
+echo git@github.com:shanekeane/$1.git
 git add .
 git commit -m "$2"
 git remote set-url origin git@github.com:shanekeane/$1.git
