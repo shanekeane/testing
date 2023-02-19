d704a0fb6373755e98dca47f3fd28dea36af7e11
shanekeane <shanekeane2001@yahoo.com>
2023-02-19 00:49:31 +0000

    xyz

--- test.sh
+++ test.sh
@@ -4,5 +4,4 @@ echo commit name: $2
 git add .
 git commit -m "$2"
 git remote add origin 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@github.com/shanekeane/$2.git
-git remote -v
-git push origin main
+git push -u origin main
